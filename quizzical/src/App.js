9e88e1c9b599871d16d2f7b2bc5dc6f5220a4147
Harmeet Matharoo <hmjatt@gmail.com>
2022-10-05 13:46:49 -0700

    Update App.js

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -1,11 +1,18 @@
-import './styles/App.css';
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-          <p>Learn React</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </div>
+    );
 }
 
 export default App;
