8c4c89b4c618c88616630ecf91c54b425a84724a
Harmeet Matharoo <39014834+hmjatt@users.noreply.github.com>
2022-10-16 21:51:52 -0700

    Update App.js

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
 			<h1 className="heading">Quizzical</h1>
             
-            <h3 className="subHeading">A Trivia Game that tests your knowledge of most unimportant topics 😃</h3>
+            <h3 className="subHeading">Trivia Game that tests your knowledge of most unimportant topics 😃</h3>
 
 			<button className="startBtn">Start Quiz</button>
             
