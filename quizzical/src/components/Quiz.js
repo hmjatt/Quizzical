53caa65f90feaa698d97c74332b9762dfab253a3
Harmeet Matharoo <hmjatt@gmail.com>
2022-10-28 19:38:55 -0700

    made it responsive

--- quizzical/src/components/Quiz.js
+++ quizzical/src/components/Quiz.js
@@ -4,60 +4,49 @@ function Quiz() {
     console.log("Quiz Component");
 
     return (
-        <div>
-            <div className="quiz">
-                <h2 className="questions">
-                    How would you say goodbye in spanish?
-                </h2>
-                <div className="options">
-                    <button>Option 1</button>
-                    <button>Option 2</button>
-                    <button>Option 3</button>
-                    <button>Option 4</button>
-                </div>
-				<hr></hr>
-                <h2 className="questions">
-                    How would you say goodbye in spanish?
-                </h2>
-                <div className="options">
-                    <button>Option 1</button>
-                    <button>Option 2</button>
-                    <button>Option 3</button>
-                    <button>Option 4</button>
-                </div>
-				<hr></hr>
-                <h2 className="questions">
-                    How would you say goodbye in spanish?
-                </h2>
-                <div className="options">
-                    <button>Option 1</button>
-                    <button>Option 2</button>
-                    <button>Option 3</button>
-                    <button>Option 4</button>
-                </div>
-				<hr></hr>
-                <h2 className="questions">
-                    How would you say goodbye in spanish?
-                </h2>
-                <div className="options">
-                    <button>Option 1</button>
-                    <button>Option 2</button>
-                    <button>Option 3</button>
-                    <button>Option 4</button>
-                </div>
-				<hr></hr>
-                <h2 className="questions">
-                    How would you say goodbye in spanish?
-                </h2>
-                <div className="options">
-                    <button>Option 1</button>
-                    <button>Option 2</button>
-                    <button>Option 3</button>
-                    <button>Option 4</button>
-                </div>
-				<hr></hr>
-                <button className="check-answers">Check Answers</button>
+        <div className="quiz">
+			<h1 className="heading">Quizzical</h1>
+            <h2 className="questions">How would you say goodbye in spanish?</h2>
+            <div className="options">
+                <button>Option 1</button>
+                <button>Option 2</button>
+                <button>Option 3</button>
+                <button>Option 4</button>
             </div>
+            <hr></hr>
+            <h2 className="questions">How would you say goodbye in spanish?</h2>
+            <div className="options">
+                <button>Option 1</button>
+                <button>Option 2</button>
+                <button>Option 3</button>
+                <button>Option 4</button>
+            </div>
+            <hr></hr>
+            <h2 className="questions">How would you say goodbye in spanish?</h2>
+            <div className="options">
+                <button>Option 1</button>
+                <button>Option 2</button>
+                <button>Option 3</button>
+                <button>Option 4</button>
+            </div>
+            <hr></hr>
+            <h2 className="questions">How would you say goodbye in spanish?</h2>
+            <div className="options">
+                <button>Option 1</button>
+                <button>Option 2</button>
+                <button>Option 3</button>
+                <button>Option 4</button>
+            </div>
+            <hr></hr>
+            <h2 className="questions">How would you say goodbye in spanish?</h2>
+            <div className="options">
+                <button>Option 1</button>
+                <button>Option 2</button>
+                <button>Option 3</button>
+                <button>Option 4</button>
+            </div>
+            <hr></hr>
+            <button className="check-answers">Check Answers</button>
         </div>
     );
 }
